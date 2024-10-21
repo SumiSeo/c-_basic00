c5062eb0681b7fe275528983b10e518ff3593461
Sumi Seo <sumseo@paul-f4Br3s5.clusters.42paris.fr>
2024-10-21 10:09:19 +0200

    fix/megaphone-with-space

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,14 +1,12 @@
 #include <cstring>
 #include <iostream>
 
-// create a function to switch lower case to upper case
-
 void	to_upper(char *s)
 {
 	int i = 0;
 	while (s[i])
 	{
-		std::cout << putwchar(toupper(s[i]));
+		std::cout << (char)toupper(s[i]);
 		i++;
 	}
 };
@@ -19,7 +17,11 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 		for (int i = 1; i < argc; i++)
+		{
 			to_upper(argv[i]);
+			if (i < argc - 1)
+				std::cout << " ";
+		}
 
 	return (0);
 }
